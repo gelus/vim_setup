9fd2076cd34ffac283b008a2020a0262f9e36fe0
Benjamin Miller <benjamin.miller@niceincontact.com>
2023-11-01 10:56:37 -0600

    removing unused packages from setup

--- setup.sh
+++ setup.sh
@@ -5,10 +5,7 @@ declare -a packages=(
 "https://github.com/gregsexton/MatchTag.git"             # highlight matching HTML tags
 "https://github.com/xolox/vim-misc.git"                  # misc library
 "https://github.com/scrooloose/nerdcommenter.git"        # nifty code commenter
-"https://github.com/scrooloose/nerdtree.git"             # directory viewer
-"https://github.com/scrooloose/syntastic.git"            # syntax checker
 "https://github.com/tomtom/tlib_vim.git"                 # utility functions
-"https://github.com/MarcWeber/vim-addon-mw-utils.gitj"   # utility lib
 "https://github.com/easymotion/vim-easymotion.git"       # vim motions on crack
 "https://github.com/tpope/vim-fugitive.git"              # git wrapper for vim
 "https://github.com/garbas/vim-snipmate.git"             # snippets system!
